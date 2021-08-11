2662793ec3fb0fef3eb99b662898ccc2aa957800
anjirajardula <anjirajardula@gmail.com>
2021-08-11 14:44:39 +0530

    Added:employee salary

--- src/com/bridgelabz/emplWage.java
+++ src/com/bridgelabz/emplWage.java
@@ -5,11 +5,18 @@ public class emplWage {
         System.out.println("Welcome to Employee Wage Computation");
         //constant
         int IS_PRESENT = 1;
-
+        int FULL_TIME = 8;
+        int WAGE_PER_HR = 20;
+        int dailyWage = 0;
+        //calculation
         int empCheck = (int) Math.floor(Math.random() * 10) % 2;
-        if (empCheck == IS_PRESENT)
+        if (empCheck == IS_PRESENT) {
             System.out.println(" Employee is Present");
-        else
+            dailyWage = FULL_TIME * WAGE_PER_HR;
+            System.out.println("the Salary is " + dailyWage);
+        } else {
             System.out.println(" Employee is Absent");
+            System.out.println(dailyWage);
+        }
     }
 }
