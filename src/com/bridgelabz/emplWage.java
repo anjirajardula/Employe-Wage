421c6c1c3fcdd23c675e8d5285fc6a79e4a1f388
anjirajardula <anjirajardula@gmail.com>
2021-09-28 23:05:07 +0530

    Modified:refactor the code

--- src/com/bridgelabz/emplWage.java
+++ src/com/bridgelabz/emplWage.java
@@ -1,6 +1,6 @@
 package com.bridgelabz;
 
-public class emplWage {
+public class EmplWage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation");
         //constant
